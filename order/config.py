2fa00d87799e558e9a853edfa6be02acea3c3292
Fred Zheng <fzheng@confluent.com>
2021-07-31 11:26:01 -0400

    add config

---
+++ order/config.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+URL = ""
+TOKEN = ""
+
+# db file to write order to
+DB_FILE = "Y:\\new-01-16.mdb"
+
+
+'''
+    menu mapping
+
+    qmenu_menu_id : lsc_menu_id
+'''
+MENU_ITEM_MAP = {
+    # empty id
+    "" : "",
+
+    "1-1-1" : "77",
+
+    "1-1-2" : "66"
+}
